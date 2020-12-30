bc11ed39271adb95ac8ae24972ddfcefb9607b61
opensomersault <abrobinson1907@gmail.com>
2020-12-30 16:06:20 +0000

    add airline and airport mapping

--- src/Flight_Scheduler/data.py
+++ src/Flight_Scheduler/data.py
@@ -10,3 +10,24 @@ class Aircraft:
     aircraftFamily = sqa.Column(sqa.String)
     aircraftClass = sqa.Column(sqa.Integer)
     propulsion = sqa.Column(sqa.Integer)
+
+
+@sqa.orm.registry.mapped
+class Airline:
+    __tablename__ = "Airline"
+
+    airline: str = sqa.Column(sqa.String, primary_key=True)
+    airlineFullName: str = sqa.Column(sqa.String)
+
+
+@sqa.orm.registry.mapped
+class Airport:
+    __tablename__ = "Airport"
+
+    airportCode: str = sqa.Column(sqa.String, primary_key=True)
+    summerTime: str = sqa.Column(sqa.String)
+    winterTime: str = sqa.Column(sqa.String)
+    city: str = sqa.Column(sqa.String)
+    state: str = sqa.Column(sqa.String)
+    country: str = sqa.Column(sqa.String)
+    fullName: str = sqa.Column(sqa.String)
