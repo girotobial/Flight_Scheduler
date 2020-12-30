ac23656db7cd3336341436c5095d08432e882882
opensomersault <abrobinson1907@gmail.com>
2020-12-30 18:35:13 +0000

    refactor into pydantic dataclasses

--- src/Flight_Scheduler/data.py
+++ src/Flight_Scheduler/data.py
@@ -1,59 +1,115 @@
 import sqlalchemy as sqa
-from sqlalchemy.ext.declarative import declarative_base
+from pydantic.dataclasses import dataclass
+from sqlalchemy.orm import registry  # type: ignore
 
-Base = declarative_base()
-
-
-class Aircraft(Base):
-    __tablename__ = "Aircraft"
-
-    aircraft = sqa.Column(sqa.String, primary_key=True)
-    fullName = sqa.Column(sqa.String)
-    aircraftFamily = sqa.Column(sqa.String)
-    aircraftClass = sqa.Column(sqa.Integer)
-    propulsion = sqa.Column(sqa.Integer)
+mapper_registry = registry()
 
 
-class Airline(Base):
-    __tablename__ = "Airline"
+@mapper_registry.mapped
+@dataclass
+class Aircraft:
+    __table__ = sqa.Table(
+        "Aircraft",
+        mapper_registry.metadata,
+        sqa.Column("aircraft", sqa.String, primary_key=True),
+        sqa.Column("fullName", sqa.String),
+        sqa.Column("aircraftFamily", sqa.String),
+        sqa.Column("aircraftClass", sqa.Integer),
+        sqa.Column("propulsion", sqa.Integer),
+    )
 
-    airline = sqa.Column(sqa.String, primary_key=True)
-    airlineFullName = sqa.Column(sqa.String)
-
-
-class Airport(Base):
-    __tablename__ = "Airport"
-
-    airportCode = sqa.Column(sqa.String, primary_key=True)
-    summerTime = sqa.Column(sqa.String)
-    winterTime = sqa.Column(sqa.String)
-    city = sqa.Column(sqa.String)
-    state = sqa.Column(sqa.String)
-    country = sqa.Column(sqa.String)
-    fullName = sqa.Column(sqa.String)
-
-
-class Flight(Base):
-    __tablename__ = "Flight"
-
-    flightId = sqa.Column(sqa.Integer, primary_key=True)
-    airline = sqa.Column(sqa.String, sqa.ForeignKey("Airlines.airline"))
-    flightNum = sqa.Column(sqa.Integer)
-    summer = sqa.Column(sqa.Integer)
-    year = sqa.Column(sqa.Integer)
-    flightComment = sqa.Column(sqa.String)
+    aircraft: str
+    fullName: str
+    aircraftFamily: str
+    aircraftClass: int
+    propulsion: int
 
 
-class Leg(Base):
+@mapper_registry.mapped
+@dataclass
+class Airline:
+    __table__ = sqa.Table(
+        "Airline",
+        mapper_registry.metadata,
+        sqa.Column("airline", sqa.String, primary_key=True),
+        sqa.Column("airlineFullName", sqa.String),
+    )
+
+    airline: str
+    airlineFullName: str
+
+
+@mapper_registry.mapped
+@dataclass
+class Airport:
+    __table__ = sqa.Table(
+        "Airport",
+        mapper_registry.metadata,
+        sqa.Column("airportCode", sqa.String, primary_key=True),
+        sqa.Column("summerTime", sqa.String),
+        sqa.Column("winterTime", sqa.String),
+        sqa.Column("city", sqa.String),
+        sqa.Column("country", sqa.String),
+        sqa.Column("fullName", sqa.String),
+    )
+
+    airportCode: str
+    summerTime: str
+    winterTime: str
+    city: str
+    state: str
+    country: str
+    fullName: str
+
+
+@mapper_registry.mapped
+@dataclass
+class Flight:
+    __table__ = sqa.Table(
+        "Flight",
+        mapper_registry.metadata,
+        sqa.Column("flightId", sqa.Integer, primary_key=True),
+        sqa.Column("airline", sqa.String, sqa.ForeignKey("Airlines.airline")),
+        sqa.Column("flightNum", sqa.Integer),
+        sqa.Column("summer", sqa.Integer),
+        sqa.Column("year", sqa.Integer),
+        sqa.Column("flightComment", sqa.String),
+    )
+
+    flightId: int
+    airline: str
+    flightNum: int
+    summer: int
+    year: int
+    flightComment: str
+
+
+@mapper_registry.mapped
+@dataclass
+class Leg:
     __tablename__ = "Leg"
+    __table__ = sqa.Table(
+        "Leg",
+        mapper_registry.metadata,
+        sqa.Column("LegID", sqa.Integer, primary_key=True),
+        sqa.Column("flightID", sqa.Integer),
+        sqa.Column("origin", sqa.String, sqa.ForeignKey("Airport.airportCode")),
+        sqa.Column("destination", sqa.String, sqa.ForeignKey("Airport.airportCode")),
+        sqa.Column("departureTime", sqa.Integer),
+        sqa.Column("arrivalTime", sqa.Integer),
+        sqa.Column("duration", sqa.Integer),
+        sqa.Column("registration", sqa.String),
+        sqa.Column("details", sqa.String),
+        sqa.Column("aircraft", sqa.String, sqa.ForeignKey("Aircraft.aircraft")),
+    )
 
-    legID = sqa.Column(sqa.Integer, primary_key=True)
-    flightId = sqa.Column(sqa.Integer)
-    origin = sqa.Column(sqa.String, sqa.ForeignKey("Airport.airportCode"))
-    destination = sqa.Column(sqa.String, sqa.ForeignKey("Airport.airportCode"))
-    departureTime = sqa.Column(sqa.Integer)
-    arrivalTime = sqa.Column(sqa.Integer)
-    duration = sqa.Column(sqa.Integer)
-    registration = sqa.Column(sqa.String)
-    details = sqa.Column(sqa.String)
-    aircraft = sqa.Column(sqa.String, sqa.ForeignKey("Aircraft.aircraft"))
+    legID: int
+    flightId: int
+    origin: str
+    destination: str
+    departureTime: int
+    arrivalTime: int
+    duration: int
+    registration: str
+    details: str
+    aircraft: str
