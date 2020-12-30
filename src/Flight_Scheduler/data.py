48d23a875dbf1da94ef478343479762f048e6a12
opensomersault <abrobinson1907@gmail.com>
2020-12-30 16:36:55 +0000

    fix: mapping decorator not supported in sqlalchemy 1.3

--- src/Flight_Scheduler/data.py
+++ src/Flight_Scheduler/data.py
@@ -1,8 +1,10 @@
 import sqlalchemy as sqa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-@sqa.orm.registry.mapped
-class Aircraft:
+class Aircraft(Base):
     __tablename__ = "Aircraft"
 
     aircraft = sqa.Column(sqa.String, primary_key=True)
@@ -12,22 +14,42 @@ class Aircraft:
     propulsion = sqa.Column(sqa.Integer)
 
 
-@sqa.orm.registry.mapped
-class Airline:
+class Airline(Base):
     __tablename__ = "Airline"
 
-    airline: str = sqa.Column(sqa.String, primary_key=True)
-    airlineFullName: str = sqa.Column(sqa.String)
+    airline = sqa.Column(sqa.String, primary_key=True)
+    airlineFullName = sqa.Column(sqa.String)
 
 
-@sqa.orm.registry.mapped
-class Airport:
+class Airport(Base):
     __tablename__ = "Airport"
 
-    airportCode: str = sqa.Column(sqa.String, primary_key=True)
-    summerTime: str = sqa.Column(sqa.String)
-    winterTime: str = sqa.Column(sqa.String)
-    city: str = sqa.Column(sqa.String)
-    state: str = sqa.Column(sqa.String)
-    country: str = sqa.Column(sqa.String)
-    fullName: str = sqa.Column(sqa.String)
+    airportCode = sqa.Column(sqa.String, primary_key=True)
+    summerTime = sqa.Column(sqa.String)
+    winterTime = sqa.Column(sqa.String)
+    city = sqa.Column(sqa.String)
+    state = sqa.Column(sqa.String)
+    country = sqa.Column(sqa.String)
+    fullName = sqa.Column(sqa.String)
+
+
+class Flight(Base):
+    __tablename__ = "Flight"
+
+    flightId = sqa.Column(sqa.Integer, primary_key=True)
+    airline = sqa.Column(sqa.String, sqa.ForeignKey("Airlines.airline"))
+    flightNum = sqa.Column(sqa.Integer)
+    summer = sqa.Column(sqa.Integer)
+    year = sqa.Column(sqa.Integer)
+    flightComment = sqa.Column(sqa.String)
+
+
+class Leg(Base):
+    __tablename__ = "Leg"
+
+    legID = sqa.Column(sqa.Integer, primary_key=True)
+    flightId = sqa.Column(sqa.Integer)
+    origin = sqa.Column(sqa.String, sqa.ForeignKey("Airport.airportCode"))
+    destination = sqa.Column(sqa.String, sqa.ForeignKey("Airport.airportCode"))
+    departureTime = sqa.Column(sqa.Integer)
+    arrivalTime = sqa.Column(sqa.Integer)
